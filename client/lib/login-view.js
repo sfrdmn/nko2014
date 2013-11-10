60052178dacb3d598fa8b21a01c2b56379abbc3c
Sean Fridman <fridman@mail.sfsu.edu>
2013-11-10 12:49:23 -0800

    forgot to add login view

---
+++ client/lib/login-view.js
@@ -0,0 +1,32 @@
+var inherits = require('util').inherits
+var EventEmitter = require('events').EventEmitter
+var elClass = require('element-class')
+
+var getTemplate = require('./template.js')
+
+function LoginView() {
+  this.el = getTemplate('tmpl-login')
+  this.form = this.el.querySelector('form')
+  this.button = this.el.querySelector('button')
+  this.userInput = this.el.querySelector('#username')
+  this.passInput = this.el.querySelector('#password')
+  document.body.appendChild(this.el)
+  elClass(this.el).add('fade fade-in')
+
+  this.form.addEventListener('submit', this.onSubmit.bind(this))
+  this.el.addEventListener('click', this.stopPropagation.bind(this))
+}
+inherits(LoginView, EventEmitter)
+
+LoginView.prototype.stopPropagation = function(e) {
+  e.stopPropagation()
+}
+
+LoginView.prototype.onSubmit = function(e) {
+  e.preventDefault()
+  elClass(this.el).remove('fade-in')
+  elClass(this.el).add('fade-out')
+  this.emit('login', this.userInput.value, this.passInput.value)
+}
+
+module.exports = LoginView
